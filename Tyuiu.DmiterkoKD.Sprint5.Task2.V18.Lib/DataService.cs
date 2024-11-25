60fca63b1a3ff22ea98622c7e37e1dfefc533b02
DmiterkoKD <kiradmiterko@gmail.com>
2024-11-25 19:55:05 +0500

    Task2

---
+++ Tyuiu.DmiterkoKD.Sprint5.Task2.V18.Lib/DataService.cs
@@ -0,0 +1,59 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.DmiterkoKD.Sprint5.Task2.V18.Lib
+{
+    public class DataService : ISprint5Task2V18
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string p = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+            FileInfo fileInfo = new FileInfo(p);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(p);
+            }
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length/rows;
+            for (int i = 0; i<rows; i++)
+            {
+                for (int j = 0; j<cols;j++)
+                {
+                    if (matrix[i,j]>0)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            string str = "";
+            for (int i = 0; i <rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (j!= cols -1)
+                    {
+                        str = str + matrix[i,j] + ";";
+                    }
+                    else
+                    {
+                        str = str + matrix[i, j];
+                    }
+                }
+                if (i!=rows-1)
+                {
+                    File.AppendAllText(p, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(p, str);
+                }
+                str = "";
+            }
+            
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.DmiterkoKD.Sprint5.Task2.V18/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.DmiterkoKD.Sprint5.Task2.V18.Lib;
+namespace Tyuiu.DmiterkoKD.Sprint5.Task2.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Дмитерко К.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Дмитерко Кира Дмитриевна | ИБКСб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая в введеной с клавиатуры матрицы заменит     *");
+            Console.WriteLine("* значиения и сохранить результат в текстовый файл OutPutFileTask1.txt    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            int[,] m = new int[3, 3] { {3, 8, 4},
+                                       {-5, -4, -3 },
+                                       {-9, 0, 2 } };
+            int rows = m.GetUpperBound(0) + 1;
+            int cols = m.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.WriteLine($"{m[i, j]}\t");
+                }
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(m);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+        }
+    }
+}
