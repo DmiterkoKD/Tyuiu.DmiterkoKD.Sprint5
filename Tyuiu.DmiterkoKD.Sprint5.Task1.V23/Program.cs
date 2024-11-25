dde680deefb1aba581324500a7900d563e56c9bb
DmiterkoKD <kiradmiterko@gmail.com>
2024-11-25 19:21:01 +0500

    Task1

--- Tyuiu.DmiterkoKD.Sprint5.Task1.V23/Program.cs
+++ Tyuiu.DmiterkoKD.Sprint5.Task1.V23/Program.cs
@@ -9,19 +9,21 @@ namespace Tyuiu.DmiterkoKD.Sprint5.Task1.V23
             Console.Title = "Спринт #5 | Выполнила: Дмитерко К.Д. | ИБКСб-24-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
             Console.WriteLine("* Выполнила: Дмитерко Кира Дмитриевна | ИБКСб-24-1                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычислит результут выражения при х=3 и      *");
-            Console.WriteLine("* сохранить результат в текстовый файл OutPutFileTask0.txt                *");
+            Console.WriteLine("* Написать программу, которая вычислит результут выражения на диапазоне   *");
+            Console.WriteLine("* [-5,5] и сохранить результат в текстовый файл OutPutFileTask1.txt       *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             int start = -5;
             int end = 5;
-            
+            Console.WriteLine("Startvalue" + start);
+            Console.WriteLine("Stopvalue" + end);
+
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
