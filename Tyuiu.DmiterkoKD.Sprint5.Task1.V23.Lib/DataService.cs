e285504e4155ff1d261a92965add38501587d7af
DmiterkoKD <kiradmiterko@gmail.com>
2024-11-25 19:05:24 +0500

    Task1

---
+++ Tyuiu.DmiterkoKD.Sprint5.Task1.V23.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.DmiterkoKD.Sprint5.Task1.V23.Lib
+{
+    public class DataService : ISprint5Task1V23
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string p = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            FileInfo fileInfo = new FileInfo(p);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(p);
+            }
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Cos(x) + (Math.Cos(x)) / (x + 2) - 3 * x;
+                y = Math.Round(y, 2);
+                if (x == 0)
+                {
+                    y = 0;
+                }
+                strY = Convert.ToString(y);
+                if (x != stopValue)
+                {
+                    File.AppendAllText(p, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(p, strY);
+                }
+            }
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.DmiterkoKD.Sprint5.Task1.V23/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.DmiterkoKD.Sprint5.Task1.V23.Lib;
+namespace Tyuiu.DmiterkoKD.Sprint5.Task1.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Дмитерко К.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Дмитерко Кира Дмитриевна | ИБКСб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычислит результут выражения при х=3 и      *");
+            Console.WriteLine("* сохранить результат в текстовый файл OutPutFileTask0.txt                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            int start = -5;
+            int end = 5;
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(start, end);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+        }
+    }
+}
